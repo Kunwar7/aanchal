42c42c146158c9d38c2dd98adc66d93df7c1c4f7
Kunwar7 <65164805+Kunwar7@users.noreply.github.com>
2026-05-10 21:23:51 +0530

    Add files via upload

--- responses.js
+++ responses.js
@@ -50,9 +50,9 @@ const responses = [
   {
     name: "kayeee",
 
-    memory: "she bakes me cookies and holds my hand tightly while crossing roads (yeah, i'd probably be dead without her)",
+    reason: "she bakes me cookies and holds my hand tightly while crossing roads (yeah, i'd probably be dead without her)",
 
-    reason: "THERE ARE SO MANYYYYY!!!! activa pe vroom vroom, watching horror movies and hearing her say 'penchod' for the first time hahah, bullying her along abhay, and seeing her get excited over overpriced stuff. her coming to my house just cuz she's hungry and hearing her yap so much. also that one trip to dalhousie... yeah SO MANY MOMENTS, can't decide one specific fav.",
+    memory: "THERE ARE SO MANYYYYY!!!! activa pe vroom vroom, watching horror movies and hearing her say 'penchod' for the first time hahah, bullying her along abhay, and seeing her get excited over overpriced stuff. her coming to my house just cuz she's hungry and hearing her yap so much. also that one trip to dalhousie... yeah SO MANY MOMENTS, can't decide one specific fav.",
 
     song: "last christmas",
 
@@ -66,9 +66,9 @@ const responses = [
   {
     name: "Ami",
 
-    memory: "We have grown up together but she is the exact opposite of me when it comes to actually express love for others. I've always admired the thoughtful letters, and ofc wherever she'll go, she'll bring something special with her ;)",
+    reason: "We have grown up together but she is the exact opposite of me when it comes to actually express love for others. I've always admired the thoughtful letters, and ofc wherever she'll go, she'll bring something special with her ;)",
 
-    reason: "late night gedi's and sneaking out without telling anyone (don't tell anyone)",
+    memory: "late night gedi's and sneaking out without telling anyone (don't tell anyone)",
 
     song: "Crimson and Clover (Tommy James & The Shondells)",
 
@@ -82,9 +82,9 @@ const responses = [
   {
     name: "Abhay",
 
-    memory: "Bc she is my sister",
+    reason: "Bc she is my sister",
 
-    reason: "Fighting",
+    memory: "Fighting",
 
     song: "Sunflower"
   },
@@ -92,9 +92,9 @@ const responses = [
   {
     name: "divjyot",
 
-    memory: "she is a reallyy caring person, always checks up on the people close to her. shes like super kindhearted and thoughtful, and has always made me feel heard. and ofcc her brownies are tasty asf. she puts thought into every little effort for the people she loves, and thats why the people she loves love her so much as well :p and also shes really fun to annoy lol. and oh she understands why ketchup is horrendous and how mint chocolate ice cream is a top flavour so bonus points fr. overall solid 11/10 person, +1 extra point for being my friend 👅😼",
+    reason: "she is a reallyy caring person, always checks up on the people close to her. shes like super kindhearted and thoughtful, and has always made me feel heard. and ofcc her brownies are tasty asf. she puts thought into every little effort for the people she loves, and thats why the people she loves love her so much as well :p and also shes really fun to annoy lol. and oh she understands why ketchup is horrendous and how mint chocolate ice cream is a top flavour so bonus points fr. overall solid 11/10 person, +1 extra point for being my friend 👅😼",
 
-    reason: "well its hard to single one moment out, because shes been an amazingg friend for the 2 years ive known her, and given me a ton of memories that im fs gonna look back upon. a memorable thing is when we all went to nurpur in 11th. that was the first time we really got to know each other, and became friends. bohot cute tha to see her shenanigans there and it made me realise shes actually a really fun person hahah",
+    memory: "well its hard to single one moment out, because shes been an amazingg friend for the 2 years ive known her, and given me a ton of memories that im fs gonna look back upon. a memorable thing is when we all went to nurpur in 11th. that was the first time we really got to know each other, and became friends. bohot cute tha to see her shenanigans there and it made me realise shes actually a really fun person hahah",
 
     song: "Tyler the creator - Sweet / I thought you wanted to dance",
 
@@ -102,5 +102,83 @@ const responses = [
       "div1.jpg",
       "div3.jpg"
     ]
+  },
+  
+  {
+    name: "Manthan",
+	reason: "Because she is short",
+    memory: "When she was on video call with Naiza and crying and saying ki bahut toxic hai and I thought she is talking abt her boyfriend but she was taking about studies",
+    song: "Choti si asha"
+  },
+  
+  {
+	  name: "Chitrakshi",
+	  reason: "Aanchal has been my best companion throughout. She understood and supported me in every situation. She is the real definition of true friend.",
+      memory:"Discussions and fights we had while going from bedi sir to aakash on every possible topic. From utter nonsense to some of the most important ones",
+      song: "Just the way you are"
+  },
+
+  {
+	 name: "Suvali Bhushin",
+	 reason: "cause she is my secret lesbian lover ",
+	 memory: "moment i remember is us panicking together about the amount of syllabus we have left (since 9th grade kabhi humara khatam hota he nahi)",
+	 song:"mai hoon don theme song ",
+	 photos: [
+		  "suvali1.jpg",
+		  "suvali2.jpg",
+		  "suvali3.jpg"
+		  ]
+  },
+  
+  {
+	  name: "Kashviiii",
+	  reason: "Aanchal is one of the best and the sweetest person i have ever met🥹. She has such a cute smile and a very big heart and always reminds me that there are still some people who can cheer you up in just a few seconds with their kindness and that pyaari si smile she has💕",
+	  memory: "Diwali get together pe us sitting on that ghumne vali chair and us trying to get a good picture like that",
+	  song: "Preety woman",
+	  photos: [
+			"kaashvi1.jpg",
+			"kaashvi2.jpg",
+			"kaashvi3.jpg"
+			]
+  },
+  
+  {
+	  name: "Pranjal",
+	  reason: "She’s been my constant for a literal lifetime. We’ve gone from kids worrying about seven stars and nurpur to teenagers worrying about mocks and colleges. She’s seen every version of me, the good and the bad, and she still chooses to be my best friend everyday. I’ve spent thousands of hours at her place doing absolutely nothing and i would never trade a second of it. She is a massive part of my best memories, and I can't imagine a version of my life where she isn’t right there in the seat next to me.",
+	  memory: "The night before our 10th ka english board, we were on call till 2 am absolutely losing our minds and shipping characters. Only she could turn a high-stress exam night into a memory that still makes me wheeze with laughter years later.",
+	  song: "Kabhi Kabhi Aditi",
+	  photos: [
+				"pranjal1.jpg",
+				"pranjal2.jpg",
+				"pranjal3.jpg"
+				]
+  },
+  
+  {
+	  name: "Savar",
+	  reason: "well she be amazing coz.......she just is? Bauni be taking care of every little thing in her relationships and shes honestly a real fun person to be around (especially if tiramisu is involved😝). The way she cares about people and makes everybody feel seen and comfortable says a lot about her as a person. Could work on the humour tho (ayy just kidding🙂‍↕️). Anyways I ll not say much kyunki yk very fragile dosti these days........",
+	  memory: "kyaaaaa??????????",
+	  song: "",
+	  photos:[
+		"savar1.jpg",
+		"savar2.jpg",
+		"savar3.jpg"
+		]
+  },
+  
+  {
+	  name: "The best person you know(kunwar)",
+	  reason: "The best person to exist. could have never expected us to grow this close together. There is just something about you that lights up my entire world. Very stubborn you are tho😔. and you need to overthink a lil less. But ya, aanchal you are one in a billion and whoever is friends with you is quite literally blessed. also, dont make fun of my humour. gandi. love you💕.",
+	  memory: "Umbrella situation. sorry tujhe bhigna padha😭. ooh also when we went out for ice cream and mine leaked and saari car gandi hogyi. tere sath toh har din is memorable.",
+	  song: "Fly me to the moon",
+	  photos:[
+	          "kunwar1.jpg",
+			  "kunwar2.jpg",
+			  "kunwar3.jpg"
+			  ]
   }
+
+
+	
+ 	
 ];
